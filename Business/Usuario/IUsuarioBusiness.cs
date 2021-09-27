e3e7ed011d9ccb7250d3706cdc2f3a5ea5bdac9e
Jonnathan Baquero <jonnathan.baquero301@outlook.es>
2021-09-26 23:19:17 -0500

    Add project business in app

---
+++ Business/Usuario/IUsuarioBusiness.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Usuario
+{
+    public interface IUsuarioBusiness
+    {   
+        Task<List<Entities.Usuario>> GetList();
+    }
+}
---
+++ Business/Usuario/UsuarioBusiness.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Usuario
+{
+    public class UsuarioBusiness : IUsuarioBusiness
+    {
+
+        private readonly SystemDbContext _context;
+        
+        public UsuarioBusiness(DbContext context)
+        {
+            _context = (SystemDbContext)context;
+        }      
+       
+        public async Task<List<Entities.Usuario>> GetList()
+        {
+            try
+            {
+                return await _context.Usuario.ToListAsync();
+            }
+            catch (Exception Exception)
+            {
+                throw new ApplicationException(Exception.Message);
+            }
+        }
+    }
+}
