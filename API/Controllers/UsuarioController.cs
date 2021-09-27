291d397c7fe436de7c806bc03c388dfac0e7ca6c
Jonnathan Baquero <jonnathan.baquero301@outlook.es>
2021-09-26 23:20:33 -0500

    Develop backend application, get users with swagger

---
+++ API/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Business.Usuario;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioBusiness _usuariobusiness;
+
+        public UsuarioController(IUsuarioBusiness UsuarioBusiness)
+        {
+            _usuariobusiness = UsuarioBusiness;
+        }      
+
+        [HttpGet("GetList")]
+        public async Task<IActionResult> GetList()
+        {
+            try
+            {
+                return Ok(await _usuariobusiness.GetList());
+            }
+            catch (Exception Exception)
+            {
+                return BadRequest(Exception);
+            }
+        }
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,54 @@
+using Business.Usuario;
+using DataAccess;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IUsuarioBusiness, UsuarioBusiness>();            
+            services.AddScoped<DbContext, SystemDbContext>();
+            services.AddSwaggerGen(x => { x.SwaggerDoc("v1", new OpenApiInfo { Title = "System", }); });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x => { x.SwaggerEndpoint("/swagger/v1/swagger.json", "System"); });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
