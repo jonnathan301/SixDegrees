0a73101f363079d5c597ff16a6faf3e8abc93f71
Jonnathan Baquero <jonnathan.baquero301@outlook.es>
2021-09-26 23:17:12 -0500

    Add project dataAccess

---
+++ DataAccess/SystemDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class SystemDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LENOVO-PC;Database=PruebaSD;User Id=sa;Password=sa2;");
+        }        
+    }
+}
