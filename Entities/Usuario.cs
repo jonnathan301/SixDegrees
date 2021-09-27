def6b322a606f44aa151798780514ef02aa6e441
Jonnathan Baquero <jonnathan.baquero301@outlook.es>
2021-09-26 22:36:05 -0500

    Add entities of project

---
+++ Entities/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class Usuario
+    {
+        [Key]
+        public decimal UsuId { get; set; }
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+        [MaxLength(100)]
+        public string Apellido { get; set; }
+    }
+}
